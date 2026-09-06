a9437efd13118b023e33ce52de254e2c2faed638
Viperio19 <viperio19yt@gmail.com>
2026-09-06 11:44:26 +0200

    Implement agent diggs side puzzle, have split dialogue for player 2 trade

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -147,7 +147,7 @@
 #define VAR_VOLCANION_CAVE_3F_STRENGTH_BUTTON_5          0x4079
 #define VAR_VOLCANION_CAVE_3F_STRENGTH_BUTTON_6          0x407A
 #define VAR_ROUTE128_STATE                               0x407B
-#define VAR_ROUTE129_STATE                               0x407C // Unused Var
+#define VAR_VOLCANION_CAVE_3F_STRENGTH_BUTTON_7          0x407C
 #define VAR_ROUTE130_STATE                               0x407D // Unused Var
 #define VAR_ROUTE131_STATE                               0x407E // Unused Var
 #define VAR_ROUTE132_STATE                               0x407F // Unused Var
